bd18c294e31ca226b13f66e62deb00fe9e820a8a
Anmol Nagpal <anmol@clouddrove.com>
2023-08-23 20:17:50 +0530

    feat: app module added

--- terraform/sandbox/blr1/main.tf
+++ terraform/sandbox/blr1/main.tf
@@ -270,3 +270,36 @@ module "mysql" {
     }
   ]
 }
+
+##------------------------------------------------
+## app module call
+##------------------------------------------------
+module "app" {
+  source  = "./../../_modules/app"
+  spec = [{
+    name    = "test"
+    enabled = true
+    region  = "nyc"
+    domain = {
+      name = "test.do.clouddrove.ca"
+      type = "PRIMARY"
+      zone = "do.clouddrove.ca"
+    }
+
+    static_site = {
+      name             = "blog"
+      build_command    = "bundle exec jekyll build -d ./public"
+      environment_slug = "hugo"
+      output_dir       = "/public"
+
+      git = {
+        repo_clone_url = "https://github.com/digitalocean/sample-jekyll.git"
+        branch         = "main"
+      }
+
+      routes = {
+        path = "/"
+      }
+    }
+  }]
+}
